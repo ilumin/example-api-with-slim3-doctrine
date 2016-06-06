56f4e63a72c76972b442c7b9e98f319f115b6a2a
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-06-06 11:44:21 +0700

    UPDATE doctrine config in core

--- app/core/Doctrine.php
+++ app/core/Doctrine.php
@@ -14,7 +14,7 @@ class Doctrine
      */
     public static function bootstrap(array $databaseConfig)
     {
-        $doctrineConfig = Setup::createAnnotationMetadataConfiguration($databaseConfig['entityPath'], true);
-        return EntityManager::create($databaseConfig, $doctrineConfig);
+        $doctrineConfig = Setup::createAnnotationMetadataConfiguration($databaseConfig['meta']['entityPath'], true);
+        return EntityManager::create($databaseConfig['connection'], $doctrineConfig);
     }
 }
