bc27219355ae9aa8a6159553f838a77e70962f38
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-06-08 12:38:05 +0700

    ADD middleware for json api

---
+++ app/middleware/ApiMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+
+class ApiMiddleware
+{
+    /**
+     * @param  \Psr\Http\Message\ServerRequestInterface $request  PSR7 request
+     * @param  \Psr\Http\Message\ResponseInterface      $response PSR7 response
+     * @param  callable                                 $next     Next middleware
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function __invoke($request, $response, $next)
+    {
+        /** @var ResponseInterface $response */
+        $response = $next($request, $response);
+        $newResponse = $response->withHeader('content-type', 'application/vnd.api+json');
+        return $newResponse;
+    }
+}
