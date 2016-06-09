963c6bcec69242235adf6aa1976ad89bacab3d76
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-06-09 13:39:25 +0700

    UPDATE doctrine cli

--- cli-config.php
+++ cli-config.php
@@ -4,20 +4,10 @@
  * Doctrine command helper
  */
 
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
-use Doctrine\ORM\Tools\Setup;
 
 try {
-    $databaseConfig = require 'config/database.php';
-    $doctrineConfig = Setup::createAnnotationMetadataConfiguration(
-        $databaseConfig['meta']['entityPath'],
-        $databaseConfig['meta']['auto_generate_proxies'],
-        $databaseConfig['meta']['proxy_dir'],
-        $databaseConfig['meta']['cache'],
-        true
-    );
-    $entityManager = EntityManager::create($databaseConfig['connection'], $doctrineConfig);;
+    $entityManager = require __DIR__ . '/bootstrap/doctrine.php';
     return ConsoleRunner::createHelperSet($entityManager);
 }
 catch (\Exception $e) {
